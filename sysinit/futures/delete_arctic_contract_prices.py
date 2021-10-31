e861d92d9b533c0696a667cde457a24f465c6e07
mjuhanne <marko.juhanne@gmail.com>
2021-10-31 18:12:53 +0200

    Delete arctic contract prices: Allow deleting contracts matching certain time period (currently only '<=' operator implemented)

--- sysinit/futures/delete_arctic_contract_prices.py
+++ sysinit/futures/delete_arctic_contract_prices.py
@@ -1,13 +1,36 @@
 """
 This script will delete (some or all) contract prices for given instrument
 """
+from sysobjects.contract_dates_and_expiries import contractDate
 from sysobjects.contracts import futuresContract
-from sysdata.arctic.arctic_futures_per_contract_prices import (
-    arcticFuturesContractPriceData,
-)
+from sysdata.arctic.arctic_futures_per_contract_prices import arcticFuturesContractPriceData
+from sysobjects.instruments import futuresInstrument
 from sysproduction.data.prices import get_valid_instrument_code_from_user
 
-    
+def process_contract_date_list( all_contract_dates:list, input:str ):
+    selected_dates = []
+    if input == 'ALL':
+        for date_str in all_contract_dates:
+            selected_dates.append(contractDate(date_str))
+    else:
+        dates = input.split(' ')
+        for date_entry in dates:
+            try:
+                if date_entry[:2] == '<=':
+                    comparison_date = contractDate(date_entry[2:]).first_contract_date.as_date()
+                    for date_str in all_contract_dates:
+                        cd = contractDate(date_str)
+                        if cd.first_contract_date.as_date() <= comparison_date:
+                            selected_dates.append(cd)
+                else:
+                    selected_dates.append( contractDate(date_entry) )
+            except:
+                print("Invalid contract date %s" % date_entry)
+
+    return selected_dates
+
+
+
 def delete_contract_prices_for_instrument(instrument_code:str):
     prices = arcticFuturesContractPriceData()
     contracts = prices.contracts_with_price_data_for_instrument_code(instrument_code)
@@ -15,26 +38,25 @@ def delete_contract_prices_for_instrument(instrument_code:str):
     contract_dates.sort()
     print("Available contracts: ", contract_dates)
 
-    cdate = input("Enter contract date or ALL if you want to delete all instrument contracts: ")
-    if cdate=='ALL':
-        print("Please confirm that you want to delete ALL contracts for instrument ", instrument_code)
-    else:
-        print("Please confirm that you want to delete contract",cdate," for instrument ", instrument_code)
+    print("Enter contract date(s) or ALL if you want to delete all instrument contracts.")
+    print("Contract dates can be given as a single contract, a list of contracts (separated by a space)")
+    print("or comparison (e.g. '<=19980600'). Currently only '<=' operator supported.")
+    input_str = input("Selection: ")
+
+    contract_date_list = process_contract_date_list(contract_dates, input_str)
+    print("Please confirm that you want to delete the following contracts for instrument %s:" % instrument_code)
+    print(contract_date_list)
 
     confirmation = input("Enter YES for confirmation: ")
     if confirmation=='YES':
         print("Deleting contract(s)..")
-        if cdate=='ALL':
-            try:
-                prices.delete_all_prices_for_instrument_code(instrument_code, areyousure=True)
-            except:
-                print("Could not delete prices for instrument ", instrument_code)
-        else:
-            try:
-                fc = futuresContract.from_two_strings(instrument_code, cdate)
+        try:
+            for cdate in contract_date_list:
+                instrument_object = futuresInstrument(instrument_code)
+                fc = futuresContract(instrument_object, cdate )
                 prices.delete_prices_for_contract_object(fc, areyousure=True)
-            except:
-                print("Could not delete contract ", cdate)
+        except:
+            print("Could not delete contract ", cdate)
     else:
         print("Prices NOT deleted")
 
