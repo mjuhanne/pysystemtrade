c70219180069b36d281a5b0e663fad27064f41b4
mjuhanne <marko.juhanne@gmail.com>
2021-12-05 20:47:59 +0200

    Add alternative IB connection for specified classes

--- sysbrokers/IB/ib_connection_defaults.py
+++ sysbrokers/IB/ib_connection_defaults.py
@@ -3,15 +3,14 @@ from syscore.objects import arg_not_supplied
 
 LIST_OF_IB_PARAMS = ["ib_ipaddress", "ib_port", "ib_idoffset"]
 
-
-def ib_defaults(**kwargs):
+def _get_ib_defaults(config_prefix, **kwargs):
     """
     Returns ib configuration with following precedence
     1- if passed in arguments: ipaddress, port, idoffset - use that
-    2- if defined in private_config file, use that. ib_ipaddress, ib_port, ib_idoffset
+    2- if defined in private_config file, use that. {config_prefix}ib_ipaddress, {config_prefix}ib_port, {config_prefix}ib_idoffset
     3 - if defined in system defaults file, use that
 
-    :return: mongo db, hostname, port
+    :return: hostname, port, id offset
     """
 
     # this will include defaults.yaml if not defined in private
@@ -25,7 +24,7 @@ def ib_defaults(**kwargs):
             param_value = arg_not_supplied
 
         if param_value is arg_not_supplied:
-            param_value = getattr(config, param_name)
+            param_value = getattr(config, config_prefix + param_name)
 
         output_dict[param_name] = param_value
 
@@ -35,3 +34,27 @@ def ib_defaults(**kwargs):
     idoffset = output_dict["ib_idoffset"]
 
     return ipaddress, port, idoffset
+
+
+def ib_defaults(**kwargs):
+    """
+    Returns ib configuration with following precedence
+    1- if passed in arguments: ipaddress, port, idoffset - use that
+    2- if defined in private_config file, use that. ib_ipaddress, ib_port, ib_idoffset
+    3 - if defined in system defaults file, use that
+
+    :return: hostname, port, id offset
+    """
+    return _get_ib_defaults("", **kwargs)
+
+
+def alternative_ib_defaults(**kwargs):
+    """
+    Returns alternative ib configuration with following precedence
+    1- if passed in arguments: ipaddress, port, idoffset - use that
+    2- if defined in private_config file, use that. alt_ib_ipaddress, alt_ib_port, alt_ib_idoffset
+    3 - if defined in system defaults file, use that
+
+    :return: hostname, port, id offset
+    """
+    return _get_ib_defaults("alt_", **kwargs)
